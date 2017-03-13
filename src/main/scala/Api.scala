c52be23b245ca5e2e03a7d628c659d3a949e8593
oil\Panthasit <oilzayo22820@gmail.com>
2017-03-13 18:04:10 +0700

    send email success

--- src/main/scala/Api.scala
+++ src/main/scala/Api.scala
@@ -12,40 +12,91 @@ trait SampleRoute extends HttpService {
 
   import spray.httpx.SprayJsonSupport._
   import Stuff._
+  import Email._
   import spray.http.MediaTypes
 
   implicit def executionContext: ExecutionContextExecutor = actorRefFactory.dispatcher
 
   var data = List(Stuff(1, "sample1"), Stuff(2, "sample2"))
-  val route = path("get") {
-    respondWithMediaType(MediaTypes.`application/json`) {
-      get {
-        complete(data)
+
+
+  val route = {
+    post {
+      path("send"/"email") {
+        entity(as[Email]) { email =>
+          complete(Email(email.sendTo, email.subject, email.body, email.fromName))
+        }
       }
     }
-  } ~
-    path("add") {
-      post {
-        entity(as[Stuff]) { stuff =>
-          complete(Stuff(stuff.id, stuff.data))
-        }
-      }
-    } ~
-    path("params") {
-      get {
-        parameters('req, 'opt.?) { (req, opt) =>
-          complete(s"Req: $req, Opt: $opt")
-        }
-      }
-    } ~
-    get {
-      path("pass"/ IntNumber) { index =>
-        complete{
-          index+"is passed"
-        }
-      }
-    }~
-    get {
-      complete("localhost")
-    }
+  }
+
 }
+
+//  val route = {
+//
+//    get {
+//      path("stuff") {
+//        complete("That's my stuff!")
+//      }
+//    } ~ post {
+//      path("stuff") {
+//        complete("That's my stuff!")
+//      }
+//    }
+//  }
+
+
+//    path("send/email") {
+//      post {
+//        entity(as[Email]) { email =>
+//          complete(Email(email.sendTo, email.subject, email.body, email.fromName))
+//        }
+//      } ~
+//      post {
+//        path("add") {
+//          entity(as[Stuff]) { stuff =>
+//            complete(Stuff(stuff.id, stuff.data))
+//          }
+//        }
+//      }
+//    }
+//  }
+//  val route = path("get") {
+//    respondWithMediaType(MediaTypes.`application/json`) {
+//      get {
+//        complete(data)
+//      }
+//    }
+//  } ~
+//    path("add") {
+//      post {
+//        entity(as[Stuff]) { stuff =>
+//          complete(Stuff(stuff.id, stuff.data))
+//        }
+//      }
+//    } ~
+//    path("params") {
+//      get {
+//        parameters('req, 'opt.?) { (req, opt) =>
+//          complete(s"Req: $req, Opt: $opt")
+//        }
+//      }
+//    } ~
+//    get {
+//      path("pass"/ IntNumber) { index =>
+//        complete{
+//          index+"is passed"
+//        }
+//      }
+//    }~
+//    path("send/email") {
+//      post {
+//        entity(as[Email]) { email =>
+//          complete(Email(email.sendTo, email.subject, email.body, email.fromName))
+//        }
+//      }
+//    } ~
+//    get {
+//      complete("localhost")
+//    }
+
--- src/main/scala/Email.scala
+++ src/main/scala/Email.scala
@@ -1,11 +1,14 @@
 import spray.json.DefaultJsonProtocol
 
+case class Email(sendTo: String, subject: String, body: String,fromName: Option[String]){
+  require(sendTo != null, "please insert sendTo")
+  require(subject != null, "please insert subject")
+  require(body != null, "please insert body")
+}
 
-case class Data(sendTo: String, subject: String, body: String)
 
+object Email extends DefaultJsonProtocol{
 
-object EmailProtocol extends DefaultJsonProtocol{
-
-  implicit val barFormat = jsonFormat3(Data)
+  implicit val emailFormat = jsonFormat4(Email.apply)
 
 }
